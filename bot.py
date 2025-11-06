4c43100043225c2d9c2904101a93baf36a0be0be
loudcor-beep <loudcor@gmail.com>
2025-11-06 09:47:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,13 @@
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-# 🔑 Вставь сюда свой токен от BotFather
 TOKEN = "8584981668:AAHm1RTtVp4nJ7TVxwG3pmfn9q--obqsSz0"
-
-# ✉️ Сообщение, которое бот будет отправлять
 AUTO_REPLY = 'Все заявки из этого чата необходимо отправлять в Битрикс24 через раздел "Автоматизация"'
 
-def reply(update, context):
-    chat_id = update.message.chat_id
-    context.bot.send_message(chat_id=chat_id, text=AUTO_REPLY)
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(AUTO_REPLY)
 
-updater = Updater(TOKEN, use_context=True)
-dp = updater.dispatcher
-dp.add_handler(MessageHandler(Filters.text & ~Filters.command, reply))
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
 
-updater.start_polling()
-updater.idle()
+app.run_polling()
