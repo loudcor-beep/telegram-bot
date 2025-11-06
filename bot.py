eca8f2cfda24a5810635d3056c3e2b129fe2ec06
loudcor-beep <loudcor@gmail.com>
2025-11-06 09:29:12 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+from telegram.ext import Updater, MessageHandler, Filters
+
+# 🔑 Вставь сюда свой токен от BotFather
+TOKEN = "8584981668:AAHm1RTtVp4nJ7TVxwG3pmfn9q--obqsSz0"
+
+# ✉️ Сообщение, которое бот будет отправлять
+AUTO_REPLY = "Все заявки из этого чата необходимо отправлять в Битрикс24 через раздел "Автоматизация"
+
+def reply(update, context):
+    chat_id = update.message.chat_id
+    context.bot.send_message(chat_id=chat_id, text=AUTO_REPLY)
+
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+dp.add_handler(MessageHandler(Filters.text & ~Filters.command, reply))
+
+updater.start_polling()
+updater.idle()
